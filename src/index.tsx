65dd8b4d62c12426342d5fdc7fd3bec8a2d32d2a
flatypus [mac] <yhc3141@gmail.com>
2024-05-02 17:32:45 -0700

    feat: renders map

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,35 @@
-import * as React from 'react';
 import './tailwind.css';
+import { spiral } from './lib/spiral';
+import { useEffect, useRef, useState } from 'react';
+import * as React from 'react';
 
-type OfflineMapProps = {
+type MapProps = {
   latitude: number;
   longitude: number;
   zoom: number;
 };
 
-const defaultOfflineMapProps: OfflineMapProps = {
+const defaultOfflineMapProps: MapProps = {
   latitude: 49.2827,
   longitude: -123.1207,
   zoom: 12,
 };
+
+type CanvasSize = {
+  width: number;
+  height: number;
+};
+
+type OfflineMapProps = Partial<
+  MapProps & { className: string; style: React.CSSProperties }
+>;
+
+const TILE_SIZE = 256;
+
+const MINIMUM_X = 800;
+const MINIMUM_Y = 600;
+
+const BUFFER = 5;
 
 function LatLngToOSM(lat: number, lng: number, zoom: number) {
   const lat_rad = (lat * Math.PI) / 180;
@@ -22,15 +40,100 @@ function LatLngToOSM(lat: number, lng: number, zoom: number) {
   return { x_tile, y_tile };
 }
 
+function renderMap(
+  canvasReference: HTMLCanvasElement,
+  x_tile: number,
+  y_tile: number,
+  zoom: number,
+  canvasSize: CanvasSize
+) {
+  const ctx = canvasReference.getContext('2d');
+  if (!ctx) return;
+
+  ctx.fillStyle = '#FFFFFF';
+  ctx.fillRect(0, 0, canvasSize.width, canvasSize.height);
+
+  const drawImage = (x: number, y: number) => {
+    const img = new Image();
+    img.onload = () => {
+      const dx = (x - x_tile) * TILE_SIZE + canvasSize.width / 2;
+      const dy = (y - y_tile) * TILE_SIZE + canvasSize.height / 2;
+      ctx.drawImage(
+        img,
+        dx - TILE_SIZE / 2,
+        dy - TILE_SIZE / 2,
+        TILE_SIZE,
+        TILE_SIZE
+      );
+    };
+    img.src = `https://a.tile.openstreetmap.org/${zoom}/${x}/${y}.png`;
+  };
+
+  Promise.allSettled(
+    spiral(
+      Math.ceil(canvasSize.width / TILE_SIZE) + BUFFER,
+      Math.ceil(canvasSize.height / TILE_SIZE) + BUFFER
+    ).map(cell => drawImage(x_tile + cell[0], y_tile + cell[1]))
+  );
+}
+
 function OfflineMap(props: Partial<OfflineMapProps>) {
-  const { latitude, longitude, zoom } = { ...defaultOfflineMapProps, ...props };
-  const { x_tile, y_tile } = LatLngToOSM(latitude, longitude, zoom);
+  const {
+    latitude: initialLatitude,
+    longitude: initialLongitude,
+    zoom: initialZoom,
+    className,
+    style,
+  } = { ...defaultOfflineMapProps, ...props };
+
+  const canvasReference = useRef<HTMLCanvasElement>(null);
+  const parentReference = useRef<HTMLDivElement>(null);
+
+  const { x_tile, y_tile } = LatLngToOSM(
+    initialLatitude,
+    initialLongitude,
+    initialZoom
+  );
+
+  const [latitude, setLatitude] = useState(initialLatitude);
+  const [longitude, setLongitude] = useState(initialLongitude);
+  const [zoom, setZoom] = useState(initialZoom);
+
+  () => {
+    console.log(latitude, longitude, zoom, setLatitude, setLongitude, setZoom);
+  };
+
+  const reload = () => {
+    if (!canvasReference.current || !parentReference.current) return;
+    console.log(
+      parentReference.current.clientWidth,
+      parentReference.current.clientHeight
+    );
+    const newClientWidth = parentReference.current?.clientWidth || 0;
+    const newClientHeight = parentReference.current?.clientHeight || 0;
+    const newSize = {
+      width: newClientWidth < MINIMUM_X ? MINIMUM_X : newClientWidth,
+      height: newClientHeight < MINIMUM_Y ? MINIMUM_Y : newClientHeight,
+    };
+    canvasReference.current.width = newSize.width;
+    canvasReference.current.height = newSize.height;
+    renderMap(canvasReference.current, x_tile, y_tile, zoom, newSize);
+  };
+
+  useEffect(() => {
+    reload();
+    window.addEventListener('resize', reload);
+    return () => window.removeEventListener('resize', reload);
+  }, [latitude, longitude, zoom]);
 
   return (
-    <img
-      src={`https://a.tile.openstreetmap.org/${zoom}/${x_tile}/${y_tile}.png`}
-      alt="map"
-    />
+    <div
+      ref={parentReference}
+      className={`${className} w-full h-full`}
+      style={style}
+    >
+      <canvas ref={canvasReference} onResize={reload}></canvas>
+    </div>
   );
 }
 
---
+++ src/lib/spiral.ts
@@ -0,0 +1,33 @@
+export function spiral(width: number, height: number) {
+  const cells: any[] = [];
+  let x = 0,
+    y = 0,
+    dx = 0,
+    dy = -1,
+    direction_switch = true;
+
+  for (let i = 0; i < width * height; i++) {
+    if (
+      -width / 2 < x &&
+      x <= width / 2 &&
+      -height / 2 < y &&
+      y <= height / 2
+    ) {
+      cells.push([x, y]);
+    }
+
+    if (
+      (direction_switch && x === y) ||
+      (x < 0 && x === -y) ||
+      (x > 0 && x === 1 - y)
+    ) {
+      [dx, dy] = [-dy, dx];
+      direction_switch = x !== y || dx === -1;
+    }
+
+    x += dx;
+    y += dy;
+  }
+
+  return cells;
+}

ed3c525f0c84c0b885da17045a22b629752c8057
flatypus [mac] <yhc3141@gmail.com>
2024-05-03 21:42:18 -0700

    chore: package.json and example

--- example/index.tsx
+++ example/index.tsx
@@ -1,13 +1,34 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import { Map } from '../src';
 
 const App = () => {
   return (
-    <div>
-      <Thing />
-    </div>
+    <Map
+      config={{
+        showCoordinates: true,
+        showCenter: true,
+      }}
+      latitude={49.541125}
+      longitude={-112.15398}
+      zoom={12}
+      className="min-h-screen w-screen"
+      mapElements={[
+        {
+          element: (
+            <img
+              className="translate-y-[-50%]"
+              width={40}
+              height={32}
+              src="https://upload.wikimedia.org/wikipedia/commons/9/9e/Pin-location.png"
+            />
+          ),
+          latitude: 49.541125,
+          longitude: -112.15398,
+        },
+      ]}
+    />
   );
 };
 
